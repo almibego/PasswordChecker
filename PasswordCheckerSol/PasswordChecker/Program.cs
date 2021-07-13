f504a406825c19e8ea623a0980465dc1c08bd635
Aleksei Mikhailov <aymikhailov@outlook.com>
2021-07-13 15:29:58 +0300

    Project added

---
+++ PasswordCheckerSol/PasswordChecker/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "1234567890";
+            string specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+            int score = 0;
+
+            Console.WriteLine("Please enter a password you would like to use:");
+            string proposedPassword = Console.ReadLine();
+
+            if (proposedPassword.Length >= minLength)
+            {
+                score += 1;
+            }
+
+            if (Tools.Contains(proposedPassword, uppercase))
+            {
+                score += 1;
+            }
+
+            if (Tools.Contains(proposedPassword, lowercase))
+            {
+                score += 1;
+            }
+
+            if (Tools.Contains(proposedPassword, digits))
+            {
+                score += 1;
+            }
+
+            if (Tools.Contains(proposedPassword, specialChars))
+            {
+                score += 1;
+            }
+
+            if (proposedPassword == "password" || proposedPassword == "1234")
+            {
+                score = 0;
+            }
+
+            switch (score)
+            {
+                case 1:
+                    Console.WriteLine("The password is weak.");
+                    break;
+                case 2:
+                    Console.WriteLine("The password is medium.");
+                    break;
+                case 3:
+                    Console.WriteLine("The password is strong.");
+                    break;
+                case 4:
+                    Console.WriteLine("The password is extremely strong.");
+                    break;
+                case 5:
+                    Console.WriteLine("The password is extremely strong.");
+                    break;
+                default:
+                    Console.WriteLine("The password does not meet any of the standards.");
+                    break;
+            }
+        }
+    }
+}
---
+++ PasswordCheckerSol/PasswordChecker/Tools.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    public class Tools
+    {
+        public Tools()
+        {
+        }
+
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+
+        public static void ContainsTest()
+        {
+            string uppercaseWord = "LASDAD";
+            string lowercaseWord = "pssst";
+            string mixedWord = "lkaAWEkasfkEW";
+
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine("Running \"tests...\"");
+            Console.WriteLine($"Should be true: {Contains(uppercaseWord, uppercase)}");
+            Console.WriteLine($"Should be false: {Contains(uppercaseWord, lowercase)}");
+            Console.WriteLine($"Should be false: {Contains(lowercaseWord, uppercase)}");
+            Console.WriteLine($"Should be true: {Contains(lowercaseWord, lowercase)}");
+            Console.WriteLine($"Should be true: {Contains(mixedWord, uppercase)}");
+            Console.WriteLine($"Should be true: {Contains(mixedWord, lowercase)}");
+        }
+    }
+}
